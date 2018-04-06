74abc118cdefa953a3fe4f8817b7f984ad0c0ffa
Cornelia <cornelia.schulz.nz@gmail.com>
2018-04-06 19:23:15 +1200

    added function to turn 2 green

--- classes.js
+++ classes.js
@@ -5,21 +5,25 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one();
     
   // Your turn! Create a new function called `two`, then call it from here.
+  two();
 }
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
-
+function two() {
+  var two = document.getElementById('two');
+  two.classList.toggle('green');
+}
 // CREATE FUNCTION three HERE
 
 // CREATE FUNCTION makeVisible HERE
